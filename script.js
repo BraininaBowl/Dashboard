2a33efdef506b5d2c5cb192c890c66fa33647770
BraininaBowl <fransvb@gmail.com>
2020-05-31 15:51:58 +0200

    Init

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  openFullscreen();
+}
+
+
+var body = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (body.requestFullscreen) {
+    body.requestFullscreen();
+  } else if (body.mozRequestFullScreen) { /* Firefox */
+    body.mozRequestFullScreen();
+  } else if (body.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    body.webkitRequestFullscreen();
+  } else if (body.msRequestFullscreen) { /* IE/Edge */
+    body.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
