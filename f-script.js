b1fbe3218231950770bbec6565f87ddc2ff31656
BraininaBowl <fransvb@gmail.com>
2020-06-04 18:17:38 +0200

    grid and time fix, basic styling.

--- f-script.js
+++ f-script.js
@@ -11,45 +11,46 @@ window.onload = function () {
 
 function startTimer() {
   function startUpdate() {
+    // we're at the first second of a minute
+    //stop the countdown
     clearInterval(startTime);
+    // update everything
     update();
-    console.log("startUpdate", time)
+    // start a new timer to run every 60 seconds
     updater = setInterval(update,60000);
   }
+  // draw stuff right now
   update();
-  console.log(time.getSeconds())
-  var startTime = setInterval(startUpdate,(60-time.getSeconds()) * 1000);
-
+  //count down towards first update at the beginning of a minute.
+  var startTime = setInterval(startUpdate,59999-time.getMilliseconds());
 }
 
 function update(){
-  time = new Date()
-
-  document.getElementById('time').innerHTML = time.toLocaleTimeString([], {hour12: false, hour: '2-digit', minute:'2-digit'});
+  getTime()
+  getPanel()
 }
 
+function getTime(){
+  time = new Date()
+  //kindle browser doesn't support "time.toLocaleTimeString([], {hour12: false, hour: '2-digit', minute:'2-digit'});" so I'm using another approach
+  document.getElementById('time').innerHTML = time.getHours() + ":" + (time.getMinutes()<10?'0':'') + time.getMinutes() + " - " + time.toLocaleDateString();
+}
 
-// Goodwe Sems solar panel api
-/*function getPanel() {
-
-  http://192.168.1.2:8080/json.htm?type=command&param=udevice&idx=$idx&nvalue=0&svalue=79
+function getPanel() {
 
 
+  /*
   var xhttp = new XMLHttpRequest();
   var baseurl = "http://192.168.178.46:8080/json.htm?"
   var options = ""
-  options += "type=devices&rid=4"
+  options += "type=command&param=udevice&idx=16&nvalue=0&svalue=POWER;ENERGY"
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("solar").innerHTML = this.responseText;
+      document.getElementById("solar").innerHTML = this.responseText.POWER;
     }
   };
   xhttp.open("GET", baseurl + options, true);
-  //xhttp.setRequestHeader("Content-type", "text/json");
-  //xhttp.setRequestHeader('token', '{"uid": "","timestamp": 0,"token": "","client": "web","version": "","language": "zh-CN" }');
   xhttp.send();
-
-
-
-}*/
+  */
+}
