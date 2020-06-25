c46d48693d32ffafef700081ed6b48719187df9f
Brain in a Bowl <fransvb@gmail.com>
2020-06-25 10:55:41 +0200

    Improved syncing of panels and meter

--- f-script.js
+++ f-script.js
@@ -3,87 +3,73 @@ var startPower;
 var screenClear;
 var time = new Date();
 var baseurl = "http://192.168.178.46:8080/json.htm?";
-var lastPanelUpdate = 0;
-var lastPanelTimeRounded;
-var thisPanelTime;
+var lastPanelUpdate;
 var opbrengst_meter_watt;
-var opbrengst;
+var gebruik_meter_watt
+var opbrengst = new Array;
 var opbrengst_watt;
 var gebruik = new Array;
 var gebruik_watt;
 var netto = new Array;
 var netto_watt;
 var gas;
+var powerUpdater
 
 window.onload = function () {
-  document.getElementById("solar_here").innerHTML = "HOI!";
   startTimer();
 }
 
 function startTimer() {
-  function powerUpdate() {
-    // The panel should update any time now
-    getPanel();
-    // start a new timer to run every 60 seconds
-    powerUpdater = setInterval(getPanel,60000);
-  }
   // draw stuff right now
   getPanel();
 
-  // get last upate from panels
-  var xhttp = new XMLHttpRequest();
-  var options = "type=devices&rid=16";
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      // get update time
-      lastPanelUpdate = Number(JSON.parse(this.response).result[0].LastUpdate.split(" ").join('').split("-").join('').split(":").join(''));
-      // get rounded update time to sync with meter data
-      lastPanelTimeRounded = JSON.parse(this.response).result[0].LastUpdate.slice(0, -3);
-      var lastNumber = Number(lastPanelTimeRounded.charAt(lastPanelTimeRounded.length-1));
-      if (lastNumber >= 5) {
-        lastNumber = 5;
-      } else {
-        lastNumber = 0;
-      }
-      lastPanelTimeRounded = lastPanelTimeRounded.slice(0, -1) + lastNumber;
-
-      // Set updater
-      var milsecsTillNextUpdate = 60000 - time.getMilliseconds() + (Number(lastPanelUpdate.toString().slice(-2))*1000);
-      while (milsecsTillNextUpdate >= 60000) {
-        milsecsTillNextUpdate -= 60000;
-      }
-      startPower = setInterval(powerUpdate,milsecsTillNextUpdate);
-    }
-  };
-  xhttp.open("GET", baseurl + options, true);
-  xhttp.send();
+  // get time to next update
+  var lastNumberMinutes = Number(time.getMinutes().charAt(lastPanelTimeRounded.length-1));
+  if (lastNumberMinutes >= 5) {
+    lastNumberMinutes -= 5;
+  }
+  powerUpdater = setInterval(getPanel, 299000 - (time.getMilliseconds() + (time.getSeconds()*1000) + (lastNumberMinutes*1000*60)));
 }
 
 function getPanel() {
-  // the solar panels get updated at the lowest frequence, so we poll them first. If the panels have been updated, we'll request the meter for the other values.
   var xhttp = new XMLHttpRequest();
-  var options = "type=devices&rid=16";
+  var options = "type=graph&sensor=counter&idx=16&range=day&method=1";
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      thisPanelTime = JSON.parse(this.response).result[0].LastUpdate;
-      var thisPanelUpdate = Number(thisPanelTime.split(" ").join('').split("-").join('').split(":").join(''));
-      if (thisPanelUpdate > lastPanelUpdate) {
-          clearInterval(startPower);
-        lastPanelUpdate = thisPanelUpdate;
-        opbrengst = JSON.parse(this.response).result[0].Usage.split(" ");
-        opbrengst_watt = Number(opbrengst[0])
-        //Watt or kW?
-        if (opbrengst_watt <= 999 && opbrengst_watt >= -999) {
-          opbrengst[0] = opbrengst_watt;
-          opbrengst[1] = "Watt";
+      opbrengst_watt = 0
+      var i = 1
+      var items = JSON.parse(this.response).result
+
+      while  (opbrengst_watt == 0) {
+        if (items[items.length - i].v) {
+          thisPanelUpdate = items[items.length - i].d;
+          if (thisPanelUpdate != lastPanelUpdate) {
+            // data has updated
+            lastPanelUpdate = thisPanelUpdate;
+            opbrengst_watt = Number(items[items.length - i].v)
+            if (opbrengst_watt <= 999 && opbrengst_watt >= -999) {
+              opbrengst[0] = opbrengst_watt;
+              opbrengst[1] = "Watt";
+            } else {
+              opbrengst[0] = opbrengst_watt/1000;
+              opbrengst[1] = "kW";
+            }
+            document.getElementById("solar_here").innerHTML = "<span class='huge'>" + opbrengst[0] + " </span> <span class='large'>" + opbrengst[1].toLowerCase() + "</span>";
+            getMeterOpbrengst()
+            getMeterGebruik()
+            getGas()
+
+            // update again in five minutes
+            clearInterval(powerUpdater);
+            powerUpdater = setInterval(powerUpdate,299000);
+          } else {
+            //data hasn't updated yet, trying again in a second
+            clearInterval(powerUpdater);
+            powerUpdater = setInterval(powerUpdate,1000);
+          }
         } else {
-          opbrengst[0] = opbrengst_watt/1000;
-          opbrengst[1] = "kW";
+          i += 1;
         }
-        document.getElementById("solar_here").innerHTML = "<span class='huge'>" + opbrengst[0] + " </span> <span class='large'>" + opbrengst[1].toLowerCase() + "</span>";
-        getMeterOpbrengst()
-        getMeterGebruik()
-        getGas()
       }
     }
   };
@@ -97,9 +83,10 @@ function getMeterOpbrengst() {
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      //kindle browser doesn't support foreach, using regular for here
       var items = JSON.parse(this.response).result
       for (var i = 0; i < items.length; i++){
-        if (items[i].d == lastPanelTimeRounded) {
+        if (items[i].d == lastPanelUpdate) {
           opbrengst_meter_watt = items[i].u;
         }
       };
@@ -115,15 +102,16 @@ function getMeterGebruik() {
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      //kindle browser doesn't support foreach, using regular for here
       var items = JSON.parse(this.response).result
       for (var i = 0; i < items.length; i++){
-        if (items[i].d == lastPanelTimeRounded) {
-          netto_watt = items[i].u;
+        if (items[i].d == lastPanelUpdate) {
+          gebruik_meter_watt = items[i].u;
         }
       };
 
       // daadwerkelijk gebruik uitrekenen
-      gebruik_watt = netto_watt + opbrengst_watt - opbrengst_meter_watt;
+      gebruik_watt = gebruik_meter_watt + opbrengst_watt - opbrengst_meter_watt;
       if (gebruik_watt <= 999 && gebruik_watt >= -999) {
         gebruik[0] = gebruik_watt;
         gebruik[1] = "Watt";
