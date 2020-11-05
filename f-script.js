12fb7bcc6a45f167e6a1d29e86261ece2be31988
Brain in a Bowl <fransvb@gmail.com>
2020-11-05 17:24:49 +0100

    Fixed updating when last request failed.

--- f-script.js
+++ f-script.js
@@ -13,33 +13,35 @@ function drawAll() {
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var items = JSON.parse(this.response).result
-      //kindle browser doesn't support foreach, using regular for
-      for (var i = 0; i < items.length; i++){
-        if (items[i].idx == 19) {
-          data.usage = items[i].Data.split(/ (.*)/);
-        } else if (items[i].idx == 20) {
-          data.delivery = items[i].Data.split(/ (.*)/);
-        } else if (items[i].idx == 16) {
-          data.solar = items[i].Usage.split(/ (.*)/);
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        var items = JSON.parse(this.response).result
+        //kindle browser doesn't support foreach, using regular for
+        for (var i = 0; i < items.length; i++){
+          if (items[i].idx == 19) {
+            data.usage = items[i].Data.split(/ (.*)/);
+          } else if (items[i].idx == 20) {
+            data.delivery = items[i].Data.split(/ (.*)/);
+          } else if (items[i].idx == 16) {
+            data.solar = items[i].Usage.split(/ (.*)/);
+          }
         }
+        if (data.usage[0] == 0) {
+          data.meter[0] = data.delivery[0]
+          data.meter[1] = data.delivery[1]
+          data.meter[3] = "▲"
+        } else {
+          data.meter[0] = data.usage[0]
+          data.meter[1] = data.usage[1]
+          data.meter[3] = "▼"
+        }
+        document.getElementById("solar_here").innerHTML = "<span class='huge'><span class='unicode'>☀</span> " + Math.round(data.solar[0]) + " </span><br><span class='large'>" + data.solar[1].toLowerCase() + "</span>";
+        document.getElementById("meter_gebruik_here").innerHTML = "<span class='huge'><span class='unicode'>" + data.meter[3] + "</span> " + Math.round(data.meter[0]) + " </span><br><span class='large'>" + data.meter[1].toLowerCase() + "</span>";
       }
-      if (data.usage[0] == 0) {
-        data.meter[0] = data.delivery[0]
-        data.meter[1] = data.delivery[1]
-        data.meter[3] = "▲"
-      } else {
-        data.meter[0] = data.usage[0]
-        data.meter[1] = data.usage[1]
-        data.meter[3] = "▼"
-      }
-      document.getElementById("solar_here").innerHTML = "<span class='huge'><span class='unicode'>☀</span> " + Math.round(data.solar[0]) + " </span><br><span class='large'>" + data.solar[1].toLowerCase() + "</span>";
-      document.getElementById("meter_gebruik_here").innerHTML = "<span class='huge'><span class='unicode'>" + data.meter[3] + "</span> " + Math.round(data.meter[0]) + " </span><br><span class='large'>" + data.meter[1].toLowerCase() + "</span>";
       setTimeout(drawAll, 200);
     }
   };
-  xhttp.open("GET", baseurl + options, true);
   xhttp.ontimeout = function () { setTimeout(drawAll, 1000); }
+  xhttp.open("GET", baseurl + options, true);
   xhttp.send();
 }
